9e105b4645ea5c3daa8278a5e31badf9da86a2be
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2021-07-22 22:19:21 -0400

    Update ConvertHereStringToArraySampleScript.ps1

--- ConvertHereStringToArraySampleScript.ps1
+++ ConvertHereStringToArraySampleScript.ps1
@@ -1,3 +1,28 @@
+function Global:Convert-HString {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)] [String]$HString
+        )
+
+    <#NOTE: This function is from Ben Wilkinson - https://gallery.technet.microsoft.com/scriptcenter/917c2357-2911-4c79-bd06-ab95714de2d4#>
+
+    Begin 
+    {Write-Verbose "Converting Here-String to Array"}
+    Process 
+    {
+        $HString -split "`n" | ForEach-Object {
+            $Item = $_.trim()
+            #NOTE: below is to enable the use of hashtag to comment aka ignore #lines in your txt file...
+            if ($Item -notmatch "#")
+            {$Item}    
+        }
+    }#Process
+    End 
+    {
+        # Nothing to do here.
+    }
+}#Convert-HString
+
 $YourHereString = @"
 Beautiful String 1
 Beautiful String 2
